37858a5bba01681fffc98343da6b95a4b7c23fa1
José Antonio da Silva <99696289+joseantonio13@users.noreply.github.com>
2022-11-16 18:33:42 -0300

    commit inicial

---
+++ parte1.js
@@ -0,0 +1,22 @@
+
+
+
+/*
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+
+ao invés de fazer isso podemos fazer: */
+
+var c = 1 
+
+while (c <= 6){
+ console.log(`Passo ${c}`)
+ c++ // c = c + 1
+
+}
+
+
---
+++ parte2.js
@@ -0,0 +1,9 @@
+/* for (inicio; teste; Incremento){
+
+}*/
+
+
+for (var c = 1; c <= 10; c++){
+    console.log(`Passo ${c}`)
+
+}
