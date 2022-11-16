46387939e792022e763d145d7b75a8a88e453c3b
José Antonio da Silva <99696289+joseantonio13@users.noreply.github.com>
2022-11-16 18:35:08 -0300

    outros projetos em js

---
+++ DETRAN.js
@@ -0,0 +1,14 @@
+
+var vel = 70
+var dist = 2 
+
+console.log ( `a distancia é de  ${dist}  e a velocidade é de: ${vel}`);
+
+/* sempre usamos o acento do é, segurando o shift e apertando duas vezes o acento assim: `` para dizer uma coisa no console e usando Cifrão ${ nome da variavel } para aparecer a variavél junto */
+
+if (vel >= 70) {
+    console.log(`Que Velocidade é essa Junior`)
+}
+
+/* Acabamos de criar uma condição simples (simples pois não tem else/se não) com javascript */
+
